543ff66bb05d8aa146253d251a1f7301fcfc8084
Taurey Butler <taureybutler@gmail.com>
2021-01-09 02:35:42 -0500

    5. Intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+//Global Style
+import GlobalStyle from './components/GlobalStyle';
+
 //Import Pages
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );

5dc6551fd4b612c0ac19d5e6a0bd0144180beaa2
Taurey Butler <taureybutler@gmail.com>
2021-01-10 12:09:59 -0500

    9. React Router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import GlobalStyle from './components/GlobalStyle';
 
 //Import Pages
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 import Nav from './components/Nav';
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/contact" exact>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }

39789d963ae38d523bc03becae4d2488f2af5aa7
Taurey Butler <taureybutler@gmail.com>
2021-01-12 01:04:22 -0500

    12. Framer Motion

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutUs from './pages/AboutUs';
 import OurWork from './pages/OurWork';
 import ContactUs from './pages/ContactUs';
 import Nav from './components/Nav';
+import MovieDetail from './pages/MovieDetail';
 //Router
 import { Switch, Route } from 'react-router-dom';
 
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact" exact>
           <ContactUs />
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,23 +1,33 @@
 import home1 from '../img/home1.png';
-//Styled
-import styled from 'styled-components';
-import { About, Description, Image, Hide } from '../styles';
 
+import { About, Description, Image, Hide } from '../styles';
+//Framer Motion
+import { motion } from 'framer-motion';
 const AboutSection = () => {
+
+    const titleAnim = {
+        hidden: {opacity: 0},
+        show: {opacity: 1, transition: {duration: 2}}
+    };
+    const container = {
+        hidden: {x: 100},
+        show: {x: 0, transition: {duration: 0.75, ease: "easeOut", staggerChildren: 1}}
+    };
+
     return ( 
         <About>
             <Description>
-                <div className="title">
+                <motion.div variants={container} initial="hidden" animate="show" className="title">
                     <Hide>
-                        <h2>we work to make</h2>
+                        <motion.h2 variants={titleAnim}>we work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnim}>your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
                 <button>Contact Us</button>
             </Description>

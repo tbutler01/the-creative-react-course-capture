20d7631d80e40e54d4a29eab6398ad3d4e6be974
Taurey Butler <taureybutler@gmail.com>
2021-01-13 05:24:29 -0500

    18. Faq Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,42 +1,38 @@
 import styled from 'styled-components';
 import { About } from '../styles';
+import Toggle from './Toggle';
+import { AnimateSharedLayout } from 'framer-motion';
 
 const FaqSection = () => {
     return ( 
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, illo?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, illo?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, illo?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What Products Do You Offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, illo?</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <AnimateSharedLayout>
+                <Toggle title='How Do I Start?'>
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, illo?</p>
+                        </div>
+                </Toggle>
+                <Toggle title='Daily Schedule'>
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, illo?</p>
+                        </div>
+                </Toggle>
+                <Toggle title='Different Payment Methods'>
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, illo?</p>
+                        </div>
+                </Toggle>
+                <Toggle title='What Products Do You Offer?'>
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, illo?</p>
+                        </div>
+                </Toggle>
+            </AnimateSharedLayout>
         </Faq>
     );
 }
